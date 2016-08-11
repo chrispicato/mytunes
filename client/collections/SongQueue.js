c42cdc9e7c768f0110344539ad03e796526955cf
chrispicato <christopherpicato@gmail.com>
2016-08-10 20:08:07 -0700

    Plays first song in queue

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,9 +1,16 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Backbone.Collection.extend({
-
   model: SongModel,
-
   initialize: function() {
-  }
+    this.on('add', function() {
+      if (this.length === 1) {
+        this.playFirst();
+      }
+    });
+  },
+
+  playFirst: function() {
+    this.models[0].play();
+  },
 
 });
--- client/views/AppView.js
+++ client/views/AppView.js
@@ -2,7 +2,7 @@
 var AppView = Backbone.View.extend({
 
   initialize: function(params) {
-    this.playerView = new PlayerView({model: this.model.get('currentSong')});
+    this.playerView = new PlayerView({model: this.model.get('library')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
